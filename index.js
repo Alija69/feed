78ee819d56434f56e8b63d7fb0283e6aee5290a9
AlijaHasan <alija.hassan@bisresearch.com>
2023-08-26 12:16:44 +0530

    deleting log file after 30 minutes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const { port} = require('./src/config/vars');
+const { port, env} = require('./src/config/vars');
 const app = require('./src/config/express');
 
 // listen to requests
-app.listen(3003, () => console.info(`server started on port ${port}`));
+app.listen(3003, () => console.info(`${env} server started on port ${port}`));
 
 /**
  * Exports express
